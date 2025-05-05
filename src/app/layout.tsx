e97efc19f0a12d479ee8a5b314327879865bfb28
Surya <suryaarioprrama@gmail.com>
2025-05-05 21:10:41 +0700

    changing photos into webp format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 // src/app/layout.tsx
 
-import Head from "next/head";
 import { ReactNode } from "react";
 import Navbar from "./components/Navbar"; // Sesuaikan dengan path komponen Navbar
 import Footer from "./components/Footer"; // Sesuaikan dengan path komponen Footer
@@ -29,8 +28,6 @@ const Layout = ({ children, title = "defaultTitle" }: LayoutProps) => {
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         <link rel="stylesheet" href="/css/basic.css" />
         <title>{title}</title>
-
-        {/* <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> */}
       </head>
 
       <body className="min-h-screen flex flex-col">
