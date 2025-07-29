2afbbef07a233b281ad4a0b32ab66523cc762497
Surya <suryaarioprrama@gmail.com>
2025-07-29 19:06:49 +0700

    update

--- prisma/migrations/20250729102738_migration1/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Like" (
-    "id" SERIAL NOT NULL,
-    "nama" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
-);
