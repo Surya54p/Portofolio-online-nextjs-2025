5dcedec344d6d9ee6617098affe0d628b220825c
Surya <suryaarioprrama@gmail.com>
2025-08-16 18:09:25 +0700

    debug

--- src/app/api/adminAPI/portofolioManagement/[id]/route.ts
+++ src/app/api/adminAPI/portofolioManagement/[id]/route.ts
@@ -9,20 +9,19 @@ export async function GET() {
   const data = await prisma.portofolios.findMany({});
   return NextResponse.json(data);
 }
-interface Context {
-  params: {
-    id: string;
-  };
-}
 
-export async function DELETE(req: NextRequest, context: Context) {
+
+export async function DELETE(
+  req: Request,
+  context: { params: { id: string } }
+) {
   const { id } = context.params;
-  // const id: string = params.id; // primitive type string
 
   try {
-    await prisma.portofolios.delete({ where: { id: String(id) } });
+    await prisma.portofolios.delete({ where: { id } });
     return NextResponse.json({ message: "Deleted successfully" });
   } catch (err: unknown) {
     return NextResponse.json({ error: (err as Error).message }, { status: 500 });
   }
+
 }
