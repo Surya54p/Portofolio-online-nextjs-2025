5d6ba48087e92a15f415d686a4de248733193869
Surya <suryaarioprrama@gmail.com>
2025-08-16 16:10:40 +0700

    debug

--- src/app/api/adminAPI/portofolioManagement/[id]/route.ts
+++ src/app/api/adminAPI/portofolioManagement/[id]/route.ts
@@ -11,25 +11,12 @@ export async function GET() {
 }
 
 export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  const { id } = params;
+
   try {
-    const { id } = params;
-
-    if (!id) {
-      return NextResponse.json({ error: "ID is required!" }, { status: 400 });
-    }
-    const deleteData = await prisma.portofolios.delete({
-      where: { id: String(id) },
-    });
-    return NextResponse.json({ message: "Data berhasil dihapus!", deleteData });
-  } catch (error: unknown) {
-    let erorMessage = "server eror";
-
-    if (error instanceof Error) {
-      erorMessage = error.message;
-    }
-
-    return NextResponse.json({ eror: erorMessage }, { status: 500 });
-  } finally {
-    await prisma.$disconnect();
+    await prisma.portofolios.delete({ where: { id: String(id) } });
+    return NextResponse.json({ message: "Deleted successfully" });
+  } catch (err: unknown) {
+    return NextResponse.json({ error: (err as Error).message }, { status: 500 });
   }
 }
