ea0947c6f44134cf07278548b8315de3811657a5
Surya <suryaarioprrama@gmail.com>
2025-08-16 13:36:52 +0700

    debug

---
+++ src/app/api/adminAPI/portofolioManagement/route.ts
@@ -0,0 +1,9 @@
+import {  PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  const data = await prisma.portofolios.findMany({});
+  return NextResponse.json(data);
+}
