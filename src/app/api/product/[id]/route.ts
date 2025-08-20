3a7de1a2fc89e446cb97722008084c99a9c1593f
Surya <suryaarioprrama@gmail.com>
2025-08-20 23:24:08 +0700

    debug api product detail

--- src/app/api/product/[id]/route.ts
+++ src/app/api/product/[id]/route.ts
@@ -2,9 +2,9 @@ import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
+type RouteContext = { params: { id: string } };
 
-// export async function GET(request: Request, context: { params: { id: string } }) {
-export async function GET(request: Request, context: any) {
+export async function GET(request: Request, context: RouteContext) {
   const { id } = context.params;
 
   const product = await prisma.product.findUnique({ where: { id } });
@@ -13,5 +13,8 @@ export async function GET(request: Request, context: any) {
     return NextResponse.json({ error: "Produk tidak ditemukan" }, { status: 404 });
   }
 
-  return NextResponse.json(product);
+  // fallback image
+  const productSafe = { ...product, image: product.image ?? "/img/still-under-construction.png" };
+
+  return NextResponse.json(productSafe);
 }
