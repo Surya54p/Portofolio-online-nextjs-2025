ecaa8d53f4f23c574e123c9cae1f05df3d6dfae7
Surya <suryaarioprrama@gmail.com>
2025-08-21 00:05:34 +0700

    debug ui page

--- src/app/components/LikeMarquee.tsx
+++ src/app/components/LikeMarquee.tsx
@@ -24,26 +24,25 @@ export default function LikeMarquee() {
 
   return (
     <div className="flex flex-col md:flex-row border md:items-center border-gray-300 rounded-xl justify-between  mb-3 py-8 px-10 sm:mb-8 sm:px-6 sm:py-4 ">
-
       <div className=" mb-4 block">
         <span className="text-2xl font-bold ">Gracias </span> <br />
         Terimakasih buat kamu yang sudah support!🔥
       </div>
 
-      <div className="flex flex-row">
-        <div className="marqueVertical-container h-32 overflow-hidden relative">
-          <div className="marqueVertical animate-marqueeVertical text-center">
+      <div className="flex flex-col lg:flex-row  gap-2  ">
+        <div className="marqueVertical-container h-32 overflow-hidden relative ">
+          <div className="marqueVertical animate-marqueeVertical  ">
             {likes.concat(likes).map((like, index) => (
-              <span key={index} className="mx-10 text-lg font-medium text-gray-700">
+              <span key={index} className=" text-center border mb-2 px-1  rounded-xl py-1 text-lg font-medium text-gray-700">
                 {like.nama}
               </span>
             ))}
           </div>
         </div>
-        <div className="marqueVertical-container h-32 overflow-hidden relative">
+        <div className="marqueVertical-container h-32 overflow-hidden relative  hidden lg:block">
           <div className="marqueVertical animate-marqueeVertical2 text-center">
             {likes.concat(likes).map((like, index) => (
-              <span key={index} className="mx-10 text-lg font-medium text-gray-700">
+              <span key={index} className=" text-center mb-2 px-1 border rounded-xl py-1 text-lg font-medium text-gray-700">
                 {like.nama}
               </span>
             ))}
